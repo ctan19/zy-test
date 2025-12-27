dec98a368efa8cbcc586443f1d2425011ae8cdaa
ctan19 <calvintann.ct@gmail.com>
2025-12-27 19:29:44 +0700

    Add files via upload

---
+++ coba.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+type Animal struct {
+	name      string
+	voice     string
+	ableToFly bool
+}
+
+func main() {
+
+	var input string
+	fmt.Print("Masukkan nama hewan: ")
+	fmt.Scan(&input)
+
+	var animal Animal
+
+	switch input {
+	case "kucing":
+		animal = Animal{
+			name:      "kucing",
+			voice:     "Meow",
+			ableToFly: false,
+		}
+	case "burung":
+		animal = Animal{
+			name:      "burung",
+			voice:     "cuit",
+			ableToFly: true,
+		}
+	case "ayam":
+		animal = Animal{
+			name:      "ayam",
+			voice:     "kukuruyuk",
+			ableToFly: false,
+		}
+	default:
+		fmt.Println("Hewan tidak dikenal")
+		return
+	}
+
+	fmt.Println("\nName: ", animal.name)
+	fmt.Println("Suara: ", animal.voice)
+
+	switch animal.ableToFly {
+	case true:
+		fmt.Println("Hewan ini bisa terbang")
+	default:
+		fmt.Println("Hewan ini tidak bisa terbang")
+	}
+
+}
