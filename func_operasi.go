77aa072995ca02f7e7bae9324f81ba1c55f01154
ctan19 <calvintann.ct@gmail.com>
2025-12-27 19:25:17 +0700

    buat file func

---
+++ func_operasi.go
@@ -0,0 +1,66 @@
+package main
+
+import "fmt"
+
+func luasPersegiPanjang(panjang, lebar float64) float64 {
+	return panjang * lebar
+}
+func luasKelilingPersegiPanjang(panjang, lebar float64) float64 {
+	return (panjang + lebar) * 2
+}
+func kelilingSegitiga(panjang float64) float64 {
+	return panjang * 3
+}
+
+func luasSegitiga(alas, tinggi float64) float64 {
+	return (alas * tinggi) / 2
+}
+
+func main() {
+	var jenis int
+
+	fmt.Println("Pilih operasi:")
+	fmt.Println("1. Hitung luas persegi")
+	fmt.Println("2. Hitung keliling persegi")
+	fmt.Println("3. Hitung keliling segitiga")
+	fmt.Println("4. Hitung luas segitiga")
+	fmt.Print("Masukkan pilihan angka: ")
+	fmt.Scan(&jenis)
+
+	switch jenis {
+	case 1:
+		var panjang, lebar float64
+		fmt.Print("\nMasukkan panjang: ")
+		fmt.Scan(&panjang)
+		fmt.Print("Masukkan lebar: ")
+		fmt.Scan(&lebar)
+		fmt.Println("Luas persegi panjang =", luasPersegiPanjang(panjang, lebar), "cm")
+
+	case 2:
+		var panjang, lebar float64
+		fmt.Println("\nMasukkan Panjang: ")
+		fmt.Scan(&panjang)
+		fmt.Println("Masukkan lebar: ")
+		fmt.Scan(&lebar)
+		fmt.Println("Keliling persegi panjang =", luasKelilingPersegiPanjang(panjang, lebar), "cm")
+
+	case 3:
+		var panjang float64
+		fmt.Println("\nMasukkan panjang: ")
+		fmt.Scan(&panjang)
+		fmt.Println("Keliling segitiga", kelilingSegitiga(panjang), "cm")
+
+	case 4:
+		var alas, tinggi float64
+		fmt.Println("\nMasukkan alas: ")
+		fmt.Scan(&alas)
+		fmt.Println("Masukkan tinggi: ")
+		fmt.Scan(&tinggi)
+		fmt.Println("Luas segitiga", luasSegitiga(alas, tinggi), "cm")
+
+	default:
+		fmt.Println("\nPilihan tidak valid!")
+
+	}
+
+}
